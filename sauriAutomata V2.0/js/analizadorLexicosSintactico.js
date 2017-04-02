0a13f7bfdd75811ba50d6119adb8e07ef852ea5f
Juanganzo69 <juan1910@live.com.mx>
2017-04-01 22:27:04 -0500

    Versión prototipo del sintáctico

--- sauriAutomata V2.0/js/analizadorLexicosSintactico.js
+++ sauriAutomata V2.0/js/analizadorLexicosSintactico.js
@@ -1,25 +1,20 @@
 //Variables grobales : div1 y div2 son los objetos de los atributos <div></div> y componentesLexicos es la expresion regular que se evalúa.
 
+
 var div1 = document.getElementById("div1");
 var div2 = document.getElementById("div2");
 var componentesLexicos =
- /(boolean+|long+|byte+|char+|double+|float+|int+|short+|String+|do+|for+|while+|[-]|[\+]|[\/]|[%]|[\*]|[,]|[a-z]|[A-Z]|[\;]|[\[]|[\]]|[\{]|[\}]|[\(]|[\)]|[\\]|[\$]|[\#]|[\?]|[\¿]|[\!]|[\¡]|[ ]|[=])/g;
-
+ /([-]|[\+]|[\/]|[\*]|[a-z]|[A-Z]|[\[]|[\]]|[\(]|[\)]|[ ]|[=])/g;
 
 
 //Tablas de entrada
-var	palabrasReservadas = [
-		"boolean","long","byte","char","double","float",
-		"int","short","string","do","for","while"
-	];
-
 var operadoresAritmeticos = [
-		"-","%","+","/","*"
+		"-","+","/","*"
 	];
 
 var delimitadores = [
-		";","[","]","(",")","{","}","$","#","?","¿","!",
-		"¡"
+		"(",")"
+		
 	];
 var asignacion = [
 		"="
@@ -37,12 +32,20 @@ var identificadoresMayusculas = [
 	];
 
 
+
+
+
+
+
 //Evento cuando se clickea el botón analizar
 document.getElementById('enviar').addEventListener("click",function(){
     var datos = document.getElementById('datos').value;
 		if(datos !== "" && componentesLexicos.test(datos) === true){
 			//si existe algún valor de entrada realiza las operaciones
 			var resultados = mostrarResultado(datos,componentesLexicos);
+			var analisisNumeros = analizador(datos,componentesLexicos);
+			console.log(analisisNumeros);
+			//analizadorSintactico(analizador(datos,componentesLexicos),tablaSintactica);
 			var otrosDatos = otros(datos,componentesLexicos);
 			console.log("\n"+resultados+"\n");
 			div1.style.border = "3px";
@@ -90,54 +93,6 @@ function mostrarResultado(texto, componentesLexicos){
 	if(componentesLexicos.test(texto) === true){
     for (var i = 0; i <= datos.length-1; i++) {
         switch(true){
-            case (datos[i] == palabrasReservadas[0]) :
-                 
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[1]) :
-                 
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[2]) :
-                 
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[3]) :
-                
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[4]) :
-                 
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[5]) :
-                 
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[6]) :
-                 
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[7]) :
-                 
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[8]) :
-                 
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[9]) :
-                
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[10]) :
-                 
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
-            case (datos[i] == palabrasReservadas[11]) :
-                 
-                 data.push("\n"+datos[i] + " es una palabra reservada");
-            break;
             case (datos[i] == operadoresAritmeticos[0]) :
                  
                  data.push("\n"+datos[i] + " es un operador aritmético");
@@ -174,42 +129,6 @@ function mostrarResultado(texto, componentesLexicos){
                  
                 data.push("\n"+datos[i] + " es un delimitador");
             break;    
-            case (datos[i] == delimitadores[4]) :
-                 
-                data.push("\n"+datos[i] + " es un delimitador");
-            break;    
-            case (datos[i] == delimitadores[5]) :
-                
-                data.push("\n"+datos[i] + " es un delimitador");
-            break;    
-            case (datos[i] == delimitadores[6]) :
-                
-                data.push("\n"+datos[i] + " es un delimitador");
-            break;    
-            case (datos[i] == delimitadores[7]) :
-                
-                data.push("\n"+datos[i] + " es un delimitador");
-            break;
-            case (datos[i] == delimitadores[8]) :
-                
-                data.push("\n"+datos[i] + " es un delimitador");
-            break;    
-            case (datos[i] == delimitadores[9]) :
-              
-                data.push("\n"+datos[i] + " es un delimitador");
-            break;    
-            case (datos[i] == delimitadores[10]) :
-               
-                data.push("\n"+datos[i] + " es un delimitador");
-            break;    
-            case (datos[i] == delimitadores[11]) :
-             
-                data.push("\n"+datos[i] + " es un delimitador");
-            break;    
-            case (datos[i] == delimitadores[12]) :
-            
-                data.push("\n"+datos[i] + " es un delimitador");
-            break;
             case (datos[i] == identificadoresMinusculas[0]) :
   
                 data.push("\n"+datos[i] + " es un identificador");
@@ -433,8 +352,249 @@ function mostrarResultado(texto, componentesLexicos){
 
 //método que analiza las coincidencias de la expresión regular y los guarda en una pila
 function analizador(texto, componentesLexicos){
+	var datos = texto.match(componentesLexicos);
+    var data = [];
+	if(componentesLexicos.test(texto) === true){
+    for (var i = 0; i <= datos.length-1; i++) {
+        switch(true){
+            case (datos[i] == operadoresAritmeticos[0]) :
+                 data.push(1);
+            break;
+            case (datos[i] == operadoresAritmeticos[1]) :
+                 
+                data.push(2);
+            break;
+            case (datos[i] == operadoresAritmeticos[2]) :
+                
+                data.push(3);
+			break;
+            case (datos[i] == operadoresAritmeticos[3]) :
+                
+                data.push(7);
+			break;				
+            case (datos[i] == delimitadores[0]) :
+                
+                data.push(5);
+            break;
+            case (datos[i] == delimitadores[1]) :
+                
+                data.push(6);
+            case (datos[i] == identificadoresMinusculas[0]) :
+  
+                data.push(4);
+            break;
+            case (datos[i] == identificadoresMinusculas[1]) :
+             
+                data.push(4);
+            break;
+            case (datos[i] == identificadoresMinusculas[2]) :
+              
+                data.push(4);
+            break;
+            case (datos[i] == identificadoresMinusculas[3]) :
+               
+                data.push(4);
+            break;
+            case (datos[i] == identificadoresMinusculas[4]) :
+              
+                data.push(4);
+            break;
+            case (datos[i] == identificadoresMinusculas[5]) :
+         
+                data.push(4);
+            break;
+            case (datos[i] == identificadoresMinusculas[6]) :
+               
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[7]) :
+             
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[8]) :
+                 
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[9]) :
+             
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[10]) :
 
-    var datos = texto.match(componentesLexicos);
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[11]) :
+               
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[12]) :
+                
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[13]) :
+          
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[14]) :
+         
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[15]) :
+              
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[16]) :
+         
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[17]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[18]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[19]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[20]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[21]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[22]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[23]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[24]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMinusculas[25]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[0]) :
+
+                data.push(4);
+            break;
+			case (datos[i] == identificadoresMayusculas[1]) :
+
+                data.push(4);
+            break;	
+            case (datos[i] == identificadoresMayusculas[2]) :
+
+                data.push(4);
+            break;
+            case (datos[i] == identificadoresMayusculas[3]) :
+
+                data.push(4);
+            break;
+            case (datos[i] == identificadoresMayusculas[4]) :
+
+                data.push(4);
+            break;
+            case (datos[i] == identificadoresMayusculas[5]) :
+
+                data.push(4);
+            break;
+            case (datos[i] == identificadoresMayusculas[6]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[7]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[8]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[9]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[10]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[11]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[12]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[13]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[14]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[15]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[16]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[17]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[18]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[19]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[20]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[21]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[22]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[23]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[24]) :
+
+                data.push(4);
+            break;     
+            case (datos[i] == identificadoresMayusculas[25]) :
+                data.push(4);
+            break;
+			case (datos[i] == asignacion[0]) :
+                data.push(0);
+            break; 	
+            default:
+                data.push(5);
+                   }
+    	}
+		return data;
+	}
     //var myString = JSON.stringify(datos, null, 2);
     //return myString;
     return datos;
@@ -446,7 +606,44 @@ function otros(texto, componentesLexicos){
 	var dataotros = otros.split("");
 	// for 12    [1,2]
 	return dataotros;
-}
+};
+
+/*function analizadorSintactico(datosEntrada, tablaSintactica){
+	var tablaSintactica=[
+	   //0  1  2  3   4     5    6  7   8
+ 		["","","","","TE'","TE'","","",""],
+		["=TE'","","","","","", "Ɛ","","Ɛ"],
+		["","","","","FT'","FT'","","",""],
+		["Ɛ","-FT'","","","","","Ɛ","","Ɛ"],
+		["","","","","FT'","FT'","","",""],
+		["Ɛ","Ɛ","+AF'","","","","Ɛ","","Ɛ"],
+		["","","","","FT'","FT'","","",""],
+		["Ɛ","Ɛ","Ɛ","/MA'","","","Ɛ","","Ɛ"],
+		["","","","","KM'","KM'","","",""],
+		["Ɛ","Ɛ","Ɛ","Ɛ","","","Ɛ","*KM'","Ɛ"],
+		["","","","","id'","(E)","","",""]
+		];
+		
+	var pila = ["$","E"];
+	for(var i = 0; i <=pila.length-1;i++){
+		switch(pila !== ""){
+			case (pila[pila.lenghth-1] === "E") :
+                var igual = datosEntrada[i].split("").reverse();
+				pila.pop();
+				pila.push(igual);
+            break;
+			case (datosEntrada[i] == 1) :
+                var menos = datosEntrada[i].split("").reverse();
+				pila.pop();
+				pila.push(menos);
+            break;
+            default:
+               
+				   }
+		
+	}
+		
+}*/
 
 //método que limpia toda la vista generadas
 function limpiar(){
@@ -458,6 +655,6 @@ function limpiar(){
 	div2.textContent = "";
 	div2.style.border = "";	
 	div2.style.borderStyle = "";
-}
+};
            
     
---
+++ sauriAutomata V2.0/js/pila.js
@@ -0,0 +1,161 @@
+/*function Stack() {
+    var elements = [];
+ 
+    this.add = add;
+    this.pop = pop;
+    this.getTopElement = getTopElement;
+    this.hasElements = hasElements;
+    this.removeAll = removeAll;
+    this.size = size;
+ 
+    function add(element) {
+        elements.push(element);
+    }
+ 
+    function pop() {
+        return elements.pop();
+    }
+ 
+    function getTopElement() {
+        return elements[elements.length - 1];
+    }
+ 
+    function hasElements() {
+        return elements.length > 0;
+    }
+ 
+    function removeAll() {
+        elements = [];
+    }
+ 
+    function size() {
+        return elements.length;
+    }
+}
+
+
+
+var fruitsStack = new Stack();
+if (!fruitsStack.hasElements()) {
+    fruitsStack.add('Banana');
+    fruitsStack.add('Apple');
+    fruitsStack.add('Mango');
+    console.log(fruitsStack.size()); // Mostrará 3
+    var mango = fruitsStack.pop(); //Obtiene mango y lo saca de la pila, ahora solo quedan 2 elementos.
+    console.log(mango);
+    console.log(fruitsStack.getTopElement()); // Imprimirá Apple, puesto que es el elemento que quedo hasta arriba.
+    console.log(fruitsStack.size()); // Mostrará 2
+    fruitsStack.removeAll(); // Limpia la pila
+    console.log(fruitsStack.hasElements()); // Retornará falso puesto que la pila esta vacía
+}*/
+/*var cont1 = 0;
+var cont2 = 0;
+var cont3 = 0;
+var cont4 = 0;
+
+
+for(var i = 1; i <=100; i++){
+	if(i%3==0){
+		console.log("numero multiplo de 3: "+ i);
+	}
+	if(i%2 ==0){
+		cont1 = cont1 + i;
+		console.log("Numero par: "+i);
+	}
+	if(i%5 ==0){
+		console.log("Numero multiplo de 5: "+i);
+		cont2 = cont2 + i;
+
+	}
+	
+	
+}
+console.log("Suma de pares: "+cont1);
+console.log("Suma de multiplos de 5: "+cont2);
+
+console.log("------------------------------------------");
+
+for(var i = 1; i <=100; i++){
+	if(i%3==0 && i%2 ==0){
+		console.log("numero multiplo de 3: "+ i);
+		cont3 = cont3+i;
+	}
+	
+}
+console.log("Suma de pares e impares: "+cont3);*/
+
+
+var matriz=[
+	["","","","","TE'","TE'","","",""],
+	["=TE'","","","","","", "Ɛ","","Ɛ"],
+	["","","","","FT'","FT'","","",""],
+	["Ɛ","-FT'","","","","","Ɛ","","Ɛ"],
+	["","","","","FT'","FT'","","",""],
+	["Ɛ","Ɛ","+AF'","","","","Ɛ","","Ɛ"],
+	["","","","","FT'","FT'","","",""],
+	["Ɛ","Ɛ","Ɛ","/MA'","","","Ɛ","","Ɛ"],
+	["","","","","KM'","KM'","","",""],
+	["Ɛ","Ɛ","Ɛ","Ɛ","","","Ɛ","*KM'","Ɛ"],
+	["","","","","id'","(E)","","",""]
+	
+	
+];
+
+
+/*arreglofeliz = [];
+
+
+console.log(matriz);
+console.log(matriz.length);
+console.log(matriz[1][0]);
+
+arreglofeliz.push(matriz[1][0],matriz[0][0]);
+console.log(arreglofeliz);
+arreglofeliz.pop();
+console.log(arreglofeliz);*/
+
+/*
+for (var x=0; x < matriz.length; x++) {
+  for (var y=0; y < matriz[x].length; y++) {
+	      console.log(matriz[x][y]);
+    		
+									   
+  }
+}
+*/
+
+
+//for(var i = 0; i<=matriz)
+
+/*console.log(matriz[0][0] + " matriz[0][0]");
+console.log(matriz[1][0] + " matriz[1][0]");
+console.log(matriz[0][1]+ " matriz[0][1]");
+console.log(matriz);*/
+
+
+var pila = ["$","E"];
+var e = ["peto"];
+var earreglo = e[0].split("").reverse();
+console.log(earreglo);
+for(var i = 0; i <= earreglo.length-1;i++){
+	pila.push(earreglo[i]);
+}
+pila.pop();
+console.log(pila);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
